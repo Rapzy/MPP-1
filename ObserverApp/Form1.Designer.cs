08a91dcba8dfd92d4e01af88ce307ccf31a9d397
Rapzy <andrushko.egor@gmail.com>
2019-11-24 08:50:58 +0300

    Create form

--- ObserverApp/Form1.Designer.cs
+++ ObserverApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ObserverApp
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,13 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.regButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.groupLabel = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.logLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // regButton
+            // 
+            this.regButton.Location = new System.Drawing.Point(18, 241);
+            this.regButton.Name = "regButton";
+            this.regButton.Size = new System.Drawing.Size(155, 48);
+            this.regButton.TabIndex = 0;
+            this.regButton.Text = "Register";
+            this.regButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(14, 13);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(80, 20);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Full Name";
+            // 
+            // groupLabel
+            // 
+            this.groupLabel.AutoSize = true;
+            this.groupLabel.Location = new System.Drawing.Point(14, 77);
+            this.groupLabel.Name = "groupLabel";
+            this.groupLabel.Size = new System.Drawing.Size(54, 20);
+            this.groupLabel.TabIndex = 4;
+            this.groupLabel.Text = "Group";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Session start",
+            "New message",
+            "New event"});
+            this.checkedListBox1.Location = new System.Drawing.Point(18, 162);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(155, 73);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Subscribe on";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(195, 36);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(336, 253);
+            this.logTextBox.TabIndex = 7;
+            this.logTextBox.Text = "";
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.Location = new System.Drawing.Point(191, 13);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(36, 20);
+            this.logLabel.TabIndex = 8;
+            this.logLabel.Text = "Log";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(583, 319);
+            this.Controls.Add(this.logLabel);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.groupLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.regButton);
+            this.Name = "MainForm";
+            this.Text = "Observer App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button regButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label groupLabel;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.Label logLabel;
     }
 }
 
