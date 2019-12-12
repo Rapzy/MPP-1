b914206ac67c4561cb78e478a3d47bda8920193d
Rapzy <andrushko.egor@gmail.com>
2019-12-12 18:54:50 +0300

    Create DeanOffice class

---
+++ ObserverApp/DeanOffice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverApp
+{
+    public class DeanOffice
+    {
+        public delegate string EventHandler(string message);
+        public event EventHandler OnSessionStart, OnNewMessage, OnNewShedule;
+        public string SessionStart()
+        {
+            return OnSessionStart?.Invoke("Session is started"); 
+        }
+        public string NewMessage()
+        {
+            return OnNewMessage?.Invoke("You have a new message");
+        }
+        public string NewShedule()
+        {
+            return OnNewShedule?.Invoke("Shedule is updated");
+        }
+    }
+}
